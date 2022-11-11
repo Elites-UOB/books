0f9d1301eb753851423c5d8221237980e6631a2f
Hassan Alkhalidi <5357529+Gruce@users.noreply.github.com>
2022-11-11 13:26:29 +0300

    init

---
+++ components/List.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="border-2 border-[#262626] rounded flex justify-center w-min mr-4" :class="{ 'flex-col w-96' : col }">
+        <slot />
+    </div>
+</template>
+
+<script setup>
+const props = defineProps({
+    col: {
+        type: Boolean,
+        default: false,
+    }
+})
+</script>
---
+++ pages/html-book.md
@@ -0,0 +1,13 @@
+---
+title: HTML Book
+theme: apple-basic
+layout: intro
+fonts:
+  sans: "Tajawal "
+  mono: "Fira Code"
+preload: false
+hideInToc: true
+
+---
+
+# كتاب
