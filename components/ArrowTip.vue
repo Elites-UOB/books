bf10a109131501d9f7dc418abfe27f4ebbdc89cd
Hassan Alkhalidi <5357529+Gruce@users.noreply.github.com>
2022-11-13 00:43:45 +0300

    q

---
+++ components/ArrowTip.vue
@@ -0,0 +1,13 @@
+<template>
+    <div display="inline" w="full" bg="gray-200" dir="ltr" relative="~">
+        <slot />
+
+        <div flex="~" w="full" left="0" bottom="0" absolute="~">
+            <span w="full" h="2px" bg="gray-500"></span>
+        </div>
+    </div>
+</template>
+
+<script setup>
+const props = defineProps(['position'])
+</script>
--- global-bottom.vue
+++ global-bottom.vue
@@ -3,6 +3,7 @@
       translate-y="-50%"
       translate-x="-50%"
       class="absolute bottom-[10mm] left-[50%] text-black"
+      :class="[ $slidev.nav.currentRoute.meta?.hidePageNumber ? 'invisible' : '' ]"
     > 
       {{$slidev.nav.currentPage}}
     </footer>
