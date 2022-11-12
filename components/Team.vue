e070551b211e3ce6fd6ff3d3394431b2a6916ce3
Hassan Alkhalidi <5357529+Gruce@users.noreply.github.com>
2022-11-13 01:29:35 +0300

    a

---
+++ components/Team.vue
@@ -0,0 +1,17 @@
+<template>
+    <div grid="~ cols-3 gap-10">
+        <div v-for="person in team" :key="person.name" flex="~ col gap-2">
+            <span>{{person.job}}</span>
+            <span text="2xl">{{person.name}}</span>
+            <a v-if="person.website" class="text-lg" :href="person.website">{{person.website}}</a>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const props = defineProps(['src'])
+const team = ref([])
+
+fetch(`/pages/${props.src}/team.json`).then(res => res.json().then(data => team.value = data))
+</script>
---
+++ theme/layouts/subject.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="slidev-layout" h="full" flex="~ col" justify="between" :text="[$slidev.nav.currentRoute.meta?.slide?.frontmatter?.secondary ? 'gray-400' : '']">
+        <h1>
+            {{$slidev.nav.currentRoute.meta?.slide?.frontmatter?.subject}}
+        </h1>
+
+        <div h="2/3">
+            <slot />
+        </div>
+    </div>
+</template>
+
+<style scoped>
+h1 {
+    font-size: 12rem;
+    font-weight: 700;
+}
+</style>
+  
